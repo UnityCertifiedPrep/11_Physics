4b08e9d54479eb1305238195cb854627c74df86b
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-02 14:02:31 +0100

    02 Rigidbodies- Gravity and Collisions

---
+++ Assets/Physics Asset Package/Aiming/BalisticsAim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalisticsAim : MonoBehaviour {
+
+    public BalisticSolver solver;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            solver.target = hitInfo.point;
+        }
+    }
+}
