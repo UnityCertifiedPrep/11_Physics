6a4a1f7b774b6bed65516168aacf17659aa0c2eb
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-08 12:26:02 +0100

    07 Raycasts- Layer Masks

--- Assets/Physics Asset Package/Aiming/BalisticsAim.cs
+++ Assets/Physics Asset Package/Aiming/BalisticsAim.cs
@@ -14,7 +14,7 @@ public class BalisticsAim : MonoBehaviour {
 	void Update () {
         var ray = new Ray(transform.position, transform.forward);
         RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo))
+        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, 1 << 8))
         {
             solver.target = hitInfo.point;
         }
